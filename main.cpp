b76be8316abd1acefdc05d63bafb39b488a33f7f
NarayanNarayan <55064088+NarayanNarayan@users.noreply.github.com>
2021-04-07 02:30:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,20 @@
 	fd_set curr_sock,ready_sock;
 	struct timeval timeout;
 
-	struct {
-		char name[24];
+	typedef struct {
+		char  name[24];
 		char ip[16];
 		int port;
-	} user_info[5];
+		int fd;
+	}connection;
 
+
+	typedef struct {
+		char  name[24];
+		char ip[16];
+		int port;
+		int fd;
+	}connection;
 
 
 	typedef struct {
