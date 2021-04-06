5b4f7d6633ad9c39a915bcd129032eb65cc7d277
NarayanNarayan <55064088+NarayanNarayan@users.noreply.github.com>
2021-04-07 02:24:48 +0530

    Update head.h

--- head.h
+++ head.h
@@ -1,4 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
+
+#include <vector>
+#include <deque>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <pair>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <iterator>
+#include <string.h>
+#include <algorithm>
+
+#include <unistd.h> 
+#include <arpa/inet.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <errno.h>
+#include <pthread.h>
+
 using namespace std;
